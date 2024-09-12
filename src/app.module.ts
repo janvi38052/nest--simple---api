690a40eccfc2b0e94ebdcc5e64cf9828ebfaf1ef
Janvi <mungekarjanvi@gmail.com>
2024-09-12 17:09:53 +0530

    middleware and exception-filters

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module,MiddlewareConsumer, NestModule, RequestMethod } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+
 
 @Module({
   imports:[ UsersModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) { 
+    consumer 
+    .apply(LoggerMiddleware)
+    .forRoutes({ path: 'users', method: RequestMethod.GET});
+  }
+
+
+}
+
+
+
+
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,41 +1,67 @@
-import { Controller, Get, Post, Param, Body, Delete, Put } from '@nestjs/common';
-import {UsersService} from  './users.service';
-
-
+import { Controller, Get, Post, Param, Body, Delete, Put, HttpException, HttpStatus } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  private users = [];
+  private users = []; // Example user array
 
   @Get()
   findAll() {
-    return this.users; 
+    try {
+      return this.users;
+    } catch (error) {
+      throw new HttpException('Failed to fetch users', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.users.find(user => user.id === id); 
+    try {
+      const user = this.users.find(user => user.id === id);
+      if (!user) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+      return user;
+    } catch (error) {
+      throw new HttpException('Failed to fetch user', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Post()
   create(@Body() createUserDto: { id: string, name: string, email: string }) {
-    this.users.push(createUserDto); 
-    return createUserDto;
+    try {
+      this.users.push(createUserDto);
+      return createUserDto;
+    } catch (error) {
+      throw new HttpException('Failed to create user', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   @Put(':id')
   update(@Param('id') id: string, @Body() updateUserDto: { name: string, email: string }) {
-    const user = this.users.find(user => user.id === id);
-    if (user) {
+    try {
+      const user = this.users.find(user => user.id === id);
+      if (!user) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
       user.name = updateUserDto.name;
       user.email = updateUserDto.email;
+      return user;
+    } catch (error) {
+      throw new HttpException('Failed to update user', HttpStatus.INTERNAL_SERVER_ERROR);
     }
-    return user;
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    this.users = this.users.filter(user => user.id !== id); 
-    return { deleted: true };
+    try {
+      const userExists = this.users.some(user => user.id === id);
+      if (!userExists) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+      this.users = this.users.filter(user => user.id !== id);
+      return { deleted: true };
+    } catch (error) {
+      throw new HttpException('Failed to delete user', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
